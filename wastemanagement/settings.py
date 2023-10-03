8a65a82c7101602a2ae3bf00b9f6df2ee97fb60a
ChrisOnsando <chrisnemwel@gmail.com>
2023-10-03 20:35:03 +0300

    feat(project): add styling to the project

--- wastemanagement/settings.py
+++ wastemanagement/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*4ylch2e9yrq7)#=iep01lh^7p90i0h$#4r1(w*^5i5u@#rgrt"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS: List[str] = [
+    "localhost",
+    "127.0.0.1",
+]
 
 AUTH_USER_MODEL = "accounts.User"
 
